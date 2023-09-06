bd803d13cdeba2832203b4272b7a86f1d6b960dc
KatyaKhudoyarova <katahudoarova330@gmail.com>
2023-09-06 11:08:20 +0300

    first commit

---
+++ 1task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+
+int main() 
+{
+    int a=3;
+    double b=0.523;
+    double result;
+    result = (pow(sqrt(1.56*sqrt(sin(b))) / (0.8942*log(a)),0.25));
+
+  std:: cout<< result;//std:cout - виведення на консоль.
+  
+}
